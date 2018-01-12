2afc8d0fbd51233d8d4fed35986a0e3c85cb68ae
John <bramleyjl@gmail.com>
2018-01-12 16:53:50 -0500

    Added background color and game restart function

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,35 +1,58 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
+@@color = 'white'
+@@guesses = 5
+@@end_message = nil
 
 def check_guess(guess)
   guess = guess.to_i
-  if guess > SECRET_NUMBER
-    return "Way too high!" if guess > (SECRET_NUMBER + 5)
+  @@guesses -= 1
+  @@number = '???'
+  if @@guesses <= 0
+    end_game('lose') unless guess === @@secret_number
+  end
+  if guess > @@secret_number
+    if guess > (@@secret_number + 5)
+      @@color = 'red'
+      return "Way too high!"
+    end
+    @@color = 'pink'
     return "Too high!"
-  elsif guess < SECRET_NUMBER
-    return "Way too low!" if guess < (SECRET_NUMBER - 5)
+  elsif guess < @@secret_number
+    if guess < (@@secret_number - 5)
+      @@color = 'red'
+      return "Way too low!"
+    end 
+    @@color = 'pink'
     return "Too low!"
   else
-    return "Your guess is correct!"
+    @@color = 'green'
+    end_game('win')
+    return "Correct!"
   end
 end
 
-def display_answer(guess)
-  if guess.to_i == SECRET_NUMBER
-    return guess
-  else
-    return "???"
+def end_game(condition)
+  if condition === 'win'
+    @@end_message = 'You win! Game has been restarted.'
+    @@number = @@secret_number
+  elsif condition === 'lose'
+    @@end_message = 'You lose! Game has been restarted.' 
   end
+  @@secret_number = rand(100)
+  @@guesses = 5
 end
 
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  number = display_answer(guess)
   erb :index, :locals => {
-    :number => number, 
-    :message => message
+    :number => @@number, 
+    :message => message,
+    :guesses => @@guesses,
+    :color => @@color,
+    :end_message => @@end_message
   }
 end
